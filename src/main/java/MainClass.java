8479b22c62bbad78bc9586a141665fee0d5f101c
ZhulkovNikolay <wed10@yandex.ru>
2018-07-19 12:03:26 +0300

    .java

---
+++ src/main/java/MainClass.java
@@ -0,0 +1,27 @@
+//Все участники должны стартовать одновременно, несмотря на то, что на подготовку у каждого их них уходит разное время.
+//В тоннель не может заехать одновременно больше половины участников (условность).
+//Попробуйте все это синхронизировать.
+//Только после того, как все завершат гонку, нужно выдать объявление об окончании.
+//Можете корректировать классы (в т.ч. конструктор машин) и добавлять объекты классов из пакета util.concurrent.
+
+import java.util.concurrent.locks.ReentrantLock;
+
+public class MainClass {
+    public static final int CARS_COUNT = 4;
+    public static void main(String[] args) {
+         ReentrantLock lock1 = new ReentrantLock(); // TODO
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
+        Race race = new Race(new Road(60), new Tunnel(), new Road(40));
+
+        Car[] cars = new Car[CARS_COUNT];
+        for (int i = 0; i < cars.length; i++) {
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 10), lock1);
+        }
+        for (int i = 0; i < cars.length; i++) {
+            new Thread(cars[i]).start();
+
+        }
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+    }
+}
